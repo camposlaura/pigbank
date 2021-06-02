de29f6e3bbc4218dedbfb6355e02443df34928e9
Laura Campos <lauracampos@id.uff.br>
2021-06-02 18:56:23 -0300

    criação do botão

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Button from './components/Button/Button'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Button>Botão teste uhul</Button>
       <Footer />
     </div>
   );

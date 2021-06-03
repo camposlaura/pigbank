34b9947988d0ece2da76ff5879d331cc54e8ecdd
Laura Campos <lauracampos@id.uff.br>
2021-06-03 00:35:06 -0300

    criação da homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,13 +5,32 @@ import Button from '../../components/Button/Button'
 import Title from '../../components/Title/Title'
 import piggyBank from '../../assets/piggy-bank.svg'
 import people from '../../assets/people.svg'
+import './HomePage.css'
 
 function HomePage() {
     return (
         <>
             <Header />
 
-            <Title text='Fique por dentro!'/>
+            <div className='page-container'>
+
+                <div className='div-1'>
+                    <img src={piggyBank} alt="desenho de um cofre em formato de porquinho" />
+                    <div className='div-1-info'>
+                        <h2>A mais nova alternativa de banco digital chegou!</h2>
+                        <p>Feito para caber no seu bolso e otimizar<br/>seu tempo. O PigBank veio pra ficar</p>
+                        <Button>Abra a sua conta</Button>
+                    </div>
+                </div>
+
+                <Title text='Fique por dentro!'/>
+
+                <div className='div-2'>
+                    <p>Ao contrário do ditado popular, por aqui, quem se mistura<br/>com porco não come farelo! Conheça nossa plataforma<br/>exclusivamente dedicada para ampliar o seu patrimônio.</p>
+                    <img src={people} alt="desenho de pessoas com dinheiro" />
+                    <Button>Cotação Moedas</Button>
+                </div>
+            </div>
 
             <Footer />
         </>

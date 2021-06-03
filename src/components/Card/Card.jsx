21dba6ea174d33dcfd53ae56d9bc18bb97934aa8
Laura Campos <lauracampos@id.uff.br>
2021-06-03 15:58:09 -0300

    usando as cores como var nas folhas de estilo

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
+// FIXME: ajeitar esse props pra receber oq eu quero direitinho
 function Card(props) {
     return (
         <div className='card'>
 
             <div className='coin-name'>
-                <h3></h3>
-                <h3 id='initials'></h3>
+                <h3>{props.name}</h3>
+                <h3 id='initials'>{props.code}</h3>
             </div>
 
-            <div className='coins-values'>
-                <h3></h3>
-                <h3></h3>
-                <h3></h3>
+            <div className='coin-info'>
+                <h3>Máxima: {props.high}</h3>
+                <h3>Mínima: {props.low}</h3>
+                <h3>Atualizado em {props.create_date}</h3>
             </div>
             
         </div>
